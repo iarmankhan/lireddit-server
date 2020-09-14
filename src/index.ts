04fe91e4e5a61bfc8997b89790efb2498dec390e
iarmankhan <armankhan9244@gmail.com>
2020-09-14 15:50:41 +0530

    Setting up apollo server for graphql

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,30 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import mikroConfig from './mikro-orm.config';
+import express from 'express';
+import {ApolloServer} from 'apollo-server-express';
+import {buildSchema} from 'type-graphql';
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
     const orm = await MikroORM.init(mikroConfig);
     
     orm.getMigrator().up();
-    // const post = orm.em.create(Post, {
-    //     title: 'Hello World'
-    // });
 
-    // await orm.em.persistAndFlush(post);
+    const app = express();
 
-    // const posts = await orm.em.find(Post, {});
-    // console.log(posts)
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        })
+    });
 
+    apolloServer.applyMiddleware({app})
+
+    app.listen(4000, () => {
+        console.log("Server started on localhost:4000")
+    })
 }
 
 main().catch(err => {
---
+++ src/resolvers/hello.ts
@@ -0,0 +1,13 @@
+import { Query, Resolver } from "type-graphql";
+
+import {Resolver Query} from 'type-graphql'
+
+@Resolver()
+export class HelloResolver {
+
+    @Query(() => String)
+    hello() {
+        return "Hello World"
+    }
+
+} 
