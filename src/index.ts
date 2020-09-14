2cca9a968165037829ce7bc80e5c24baa4434e04
iarmankhan <armankhan9244@gmail.com>
2020-09-14 18:49:17 +0530

    Added authentication logic - user table, login - register

--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,17 @@ import {ApolloServer} from 'apollo-server-express';
 import {buildSchema} from 'type-graphql';
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
+import { UserResolver } from './resolvers/user';
 
 const main = async () => {
     const orm = await MikroORM.init(mikroConfig);
-    
-    orm.getMigrator().up();
+    await orm.getMigrator().up();
 
     const app = express(); 
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [HelloResolver, PostResolver],
+            resolvers: [HelloResolver, PostResolver, UserResolver],
             validate: false
         }),
         context: () => ({
